29a6de9c4fee1f9e272bc6337f0424d81345e73c
AlanMFlores <69022647+AlanMFlores@users.noreply.github.com>
2022-08-22 00:25:03 -0300

    Solucion error shoppingCartStorage

--- scripts/renderShoppingCart.js
+++ scripts/renderShoppingCart.js
@@ -5,9 +5,9 @@ const renderShoppingCart = () => {
     const emptyCart = document.createElement('h3');
     emptyCart.classList.add('empty-cart')
     emptyCart.innerHTML = `No hay productos en tu carrito. Puedes agregarlo desde <a href="catalogo.html">AQUÍ</a>`
-    let shoppingCartListStorage = getShoppingCartStorage();
+    let shoppingCartStorage = getShoppingCartStorage();
 
-    if(shoppingCartListStorage.length > 0) {
+    if(shoppingCartStorage.length > 0) {
         shoppingCartStorage.forEach( product => {
             let card = document.createElement('li');
             card.classList.add('shopping-cart--products-list-item')
